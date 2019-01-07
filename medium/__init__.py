e5433f254152c4190c6f283ef28176a841808fb5
florian dahlitz <f2dahlitz@freenet.de>
2019-01-07 20:31:20 +0100

    feat: added browser session to package init.py

--- medium/__init__.py
+++ medium/__init__.py
@@ -3,4 +3,37 @@ Alternative Medium API.
 
 An alternative Medium API for interacting with user specific data.
 """
+
+import platform
+
+from pathlib import Path
+from selenium import webdriver
+
+
 __version__ = "0.0.1"
+
+
+def _get_executable_path():
+    os_mapping = {
+        "Windows": "windows",
+        "Darwin": "macosx",
+        "Linux": "linux"
+    }
+
+    os_type = platform.system()
+
+    if os_type not in os_mapping:
+        raise EnvironmentError(f"Unsupported system type: {os_type}")
+
+    os_info = os_mapping[os_type]
+    filename = "phantomjs-" + os_info
+
+    if os_type == 'Windows':
+        filename += ".exe"
+
+    path = Path(__file__).resolve().parent / "phantomjs" / filename
+
+    return str(path)
+
+
+browser = webdriver.PhantomJS(executable_path=_get_executable_path())
